0e595b9b8a3980c48b5758301e2f2ce1dfb5400e
Russel Pugh <russ_scooby@yahoo.com>
2020-02-27 22:33:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array.pop.to_i(4)
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.shift.to_i(0)
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
